0510b1dd52a7983e5e43b0ae14fa42ad4c899279
Timothy Chandler <ernaldis@protonmail.com>
2021-12-03 22:09:52 -0600

    give equivalentResistance type decleration

--- src/Resistor.hs
+++ src/Resistor.hs
@@ -2,6 +2,7 @@ module Resistor where
 
 addTwo a b = a + b
 
+equivalentResistance :: [(Float, Char)] -> Float
 equivalentResistance [] = 0
 equivalentResistance [(resistance, _)] = resistance
 equivalentResistance [(a, _), (b, x)]
