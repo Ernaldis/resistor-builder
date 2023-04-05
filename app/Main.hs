017fcd96a5e3e7f3346c227a4b59b4768745e9ab
Timothy Chandler <ernaldis@protonmail.com>
2023-04-05 11:05:17 -0500

    fix tests and seperate library from executable

---
+++ app/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import ResistorBuilder (find)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let target = read (head args) :: Float
+  let result = find target 0
+  case result of
+    Just resistors -> do
+      print resistors
+    Nothing -> putStrLn "No solution found"
+
